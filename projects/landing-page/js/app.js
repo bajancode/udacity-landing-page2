438baf80ad684e5fa2a7db67f92a46aee29f8bd0
bajancode <victory.web.development@gmail.com>
2021-12-27 20:28:55 -0400

    first commit

--- projects/landing-page/js/app.js
+++ projects/landing-page/js/app.js
@@ -22,7 +22,9 @@
  * Define Global Variables
  * 
 */
-
+const navbarList = document.getElementById("navbar__list");
+const sections = document.querySelectorAll("section");
+const sectionHeadings = document.getElementsByClassName("section__heading");
 
 /**
  * End Global Variables
@@ -30,15 +32,58 @@
  * 
 */
 
-
-
 /**
  * End Helper Functions
  * Begin Main Functions
  * 
 */
 
-// build the nav
+// Build the nav
+
+function makeLi (sections) {
+    for(let i=0; i < sections.length; i++) {
+        let section = sections[i];
+        section.insertAdjacentHTML('beforebegin', "<li>");
+        section.insertAdjacentHTML('afterend', "</li>");
+}};
+
+makeLi(sections)
+
+
+
+//getting text from h2 elements, cloned so don't modify initial html. May need to rename as doing more now
+function getHeadingText(sectionHeadings) {
+    for (let i=0; i < sectionHeadings.length; i++) {
+        let sectionHeading = sectionHeadings[i]
+        let clone = sectionHeading.cloneNode(true);
+        let cloneText = clone.textContent
+        console.log(cloneText)
+        // clickNav(cloneText)
+
+        let appendedNavbar = navbarList.append(cloneText)
+
+        //wouldl ike to do the clickNav but seems to return it to Section 1 only and it doesn't work...hrm...
+        //is it b/c appendedNavbar is one list?
+        // clickNav(appendedNavbar)
+        
+    }
+
+}
+
+getHeadingText(sectionHeadings)
+
+function clickNav (ele) {
+    ele.addEventListener("click", function(event) {
+        console.log("pudddddddddding")
+
+        event.preventDefault();
+    }  )
+}
+
+
+
+
+
 
 
 // Add class 'active' to section when near top of viewport
